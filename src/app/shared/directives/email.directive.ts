e49299a7cc7d18e6b15fa881489d79cf11ec6850
Vaibhav Mishra <vaibhav_mishra@epam.com>
2024-09-10 13:11:56 +0530

    bug fixes

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,10 +1,14 @@
 import { Directive } from "@angular/core";
-import { Validator } from "@angular/forms";
+import { AbstractControl, ValidationErrors, Validator } from "@angular/forms";
 
 @Directive({
-    selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+  selector: "[appEmailValidator]",
 })
 export class EmailValidatorDirective implements Validator {
-    // Add your code here
+  validate(control: AbstractControl): ValidationErrors | null {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isValid = emailPattern.test(control.value);
+
+    return isValid ? null : { invalidEmail: true };
+  }
 }
